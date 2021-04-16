8ea1ba79d9bad1a1fd63bc644363d8617bac9b45
Shea McKinney <sherakama@gmail.com>
2021-04-16 16:26:21 -0700

    ADAPT-2296: Post css build concept idea.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,11 @@
+const autoprefixer = require('autoprefixer')
+const tailwind = require('tailwindcss');
+const postcss = require('postcss')
+const precss = require('precss')
+const fs = require('fs')
+const path = require('path');
+const fse = require('fs-extra');
+
 /**
  * On post build step for gatbsy builds.
  *
@@ -5,8 +13,48 @@
  * @param {*} pluginOptions
  */
 exports.onPostBuild = async function onPostBuild(nodeOptions, pluginOptions) {
-  console.log(JSON.stringify({
-    nodeOptions,
-    pluginOptions
-  }, null, 2))
+  const entry = path.resolve(pluginOptions.src);
+  const dest = path.resolve(pluginOptions.dest);
+  const defaultPlugins = [precss, tailwind, autoprefixer];
+  let userOpts = false;
+  if (pluginOptions.config?.length) {
+    userOpts = require(pluginOptions.config)
+  }
+  else {
+    try {
+      userOpts = require(path.resolve('./postcss.config.js'))
+    }
+    catch (error) {
+      userOpts = false;
+    }
+  }
+  const postConfig = userOpts.plugins || defaultPlugins;
+
+  fs.readFile(entry, (err, css) => {
+    postcss(postConfig)
+      .process(css, 
+        { 
+          from: entry, 
+          to: dest 
+        }
+      )
+      .then(result => {
+        fse.outputFile(dest, result.css, err => {
+          if(err) {
+            console.log(err);
+          } else {
+            console.info('The css was generated to path: ' + dest);
+          }
+        })
+
+        if ( result.map ) {
+          fs.writeFile(dest + '.map', result.map.toString(), () => true)
+        }
+      })
+  })
+
 }
+
+
+
+
